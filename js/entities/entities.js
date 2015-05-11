ae8254d85f65008f8b82d4cbf14883819462c973
kalamaboop <rosariok28@yahoo.com>
2015-05-10 20:48:29 -0700

    Update entities.js

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -27,9 +27,9 @@ var CharEntity = me.Entity.extend({
         if (this.pos.x > (me.game.viewport.width - this.width)) {
           this.pos.x = me.game.viewport.width - this.width;
         }
-        //if (this.pos.y < me.game.viewport.height/2 + 124) {
-          //this.body.update(dt);
-        //}
+        if (this.pos.y < me.game.viewport.height/2 + 124) {
+          this.body.update(dt);
+        }
         if (me.input.isKeyPressed('jump')){ //&& this.pos.y <= me.game.viewport.height/2 + 124) {
             var maxHeight = (me.game.viewport.height / 3);
             if (this.pos.y > maxHeight) {
