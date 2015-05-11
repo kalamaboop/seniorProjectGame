4b160f0ffd9b6e33e15acf74e5a18d359bb23474
kalamaboop <rosariok28@yahoo.com>
2015-05-11 14:43:46 -0700

    Update entities.js

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -25,13 +25,13 @@ var CharEntity = me.Entity.extend({
 
     update : function (dt) {
         if (this.pos.y < me.game.viewport.height/2 + 124) {
-            this.pos.y += 5;
+            this.pos.y += 5.5;
           //this.body.update(dt);
         }
         else if (me.input.isKeyPressed('jump')) {
             var currentPos = this.pos.y;
             this.jumpTween.stop();
-            this.jumpTween.to({y: currentPos - 150}, 50);
+            this.jumpTween.to({y: currentPos - 160}, 50);
             this.jumpTween.start();
         }
         
